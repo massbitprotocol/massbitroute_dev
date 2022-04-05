5c6ba1ae078c1efda112354b0310972d2d217bc6
Vu Tran <baysao@gmail.com>
2022-04-05 14:45:24 +0700

    Fix install.sh scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,12 @@ fi
 
 apt update
 apt install -y git
-git clone https://github.com/massbitprotocol/massbitroute_dev.git /massbit/massbitroute/app/src -b dev
-git clone $GIT_PRIVATE_READ_URL/massbitroute/env.git /massbit/massbitroute/app/src/env -b dev
+if [ ! -d "/massbit/massbitroute/app/src/.git" ]; then
+	git clone https://github.com/massbitprotocol/massbitroute_dev.git /massbit/massbitroute/app/src -b $BRANCH
+else
+	git -C /massbit/massbitroute/app/src pull origin $BRANCH
+fi
 
-/massbit/massbitroute/app/src/scripts/run _install
+git clone $GIT_PRIVATE_READ_URL/massbitroute/env.git /massbit/massbitroute/app/src/env -b $BRANCH
+
+bash -x /massbit/massbitroute/app/src/scripts/run _install
