990091f772d28912ce3a8e702fe8f7ec0cb96183
Vu Tran <baysao@gmail.com>
2022-04-05 14:14:18 +0700

    Add scripts install

---
+++ tests/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BRANCH=${BRANCH:-dev}
+if [ -z "$GIT_PRIVATE_READ_URL" ]; then
+	echo "Need GIT_PRIVATE_READ_URL"
+	exit 1
+fi
+
+apt update
+apt install -y git
+git clone https://github.com/massbitprotocol/massbitroute_dev.git /massbit/massbitroute/app/src -b dev
+git clone $GIT_PRIVATE_READ_URL/massbitroute/env.git /massbit/massbitroute/app/src/env -b dev
