2a7b1db3a93fe784a8addf94d39a2a75951c8473
Vu Tran <baysao@gmail.com>
2022-04-05 14:19:31 +0700

    Add scripts install

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,3 +9,5 @@ apt update
 apt install -y git
 git clone https://github.com/massbitprotocol/massbitroute_dev.git /massbit/massbitroute/app/src -b dev
 git clone $GIT_PRIVATE_READ_URL/massbitroute/env.git /massbit/massbitroute/app/src/env -b dev
+
+/massbit/massbitroute/app/src/scripts/run _install
