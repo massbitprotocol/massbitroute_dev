4e4a7c3cab3f0896c3f43ce03e5cccb65da37d92
Vu Tran <baysao@gmail.com>
2022-07-11 17:14:21 +0700

    fix mkdir

--- install/docker.sh
+++ install/docker.sh
@@ -9,6 +9,7 @@ bran=$2
 ver=$3
 shift 3
 _f=./mbr/run/docker-compose-$ver-$bran-$env.yml
+mkdir -p ./mbr/run
 sed "s/_BRANCH_/$bran/g" ./mbr/docker-compose-$env.yml >$_f
 sed "s/_VER_/$ver/g" -i $_f
 sed "s/_ENV_/$env/g" -i $_f
